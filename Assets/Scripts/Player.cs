3eb870943a233661c22b4bc13313fd3a7800b3fc
Xen <klimovjeka0987@gmail.com>
2017-12-08 17:53:01 +0300

    -Begin NavMeshAgent Units;

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,17 +7,41 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     [SerializeField]
+    private int teamNumber;
+    [SerializeField]
+    private List<GameObject> selected;//Выбранные юниты
+    [SerializeField]
+    private LayerMask movementMask;
+    [SerializeField]
     private Resource[] resources = new Resource[Enum.GetNames(typeof(ResourceType)).Length];
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
+        //Инициализация всех типов ресурсов
 		for(int i = 0; i< resources.Length; i++)
         {
             resources[i] = new Resource((ResourceType)i);
         }
+        //Инициализация номера команды
+        teamNumber = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray,out hit))
+            {
+
+            }
+        }
 	}
+
+    public void AddSelectedUnit(GameObject selectedObj, bool clear = true)
+    {
+        if (clear) selected.Clear();
+        selected.Add(selectedObj);
+    }
 }
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Unit : MonoBehaviour, IInteractable
+{
+    private int owner;
+    private Player player;
+    private NavMeshAgent agent;
+
+    public NavMeshAgent Agent
+    {
+        get
+        {
+            return agent;
+        }
+
+        set
+        {
+            agent = value;
+        }
+    }
+
+    public void Start()
+    {
+        player = Camera.main.GetComponentInParent<Player>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void OnMouseDown()
+    {
+        player.AddSelectedUnit(gameObject);
+    }
+
+    public void MoveToPoint(Vector3 targetPoint)
+    {
+        agent.SetDestination(targetPoint);
+    }
+}
